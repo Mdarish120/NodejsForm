70d4cfd2e3e61ca625866acc83b38b2786958661
Mohd Arish <arishmd768@gmail>
2023-09-14 13:12:06 +0530

    add-bodyParser

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const express=require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const app=express();
+// Use body-parser middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Set up a route to show the form
+app.get('/add-product', (req, res) => {
+  res.send(`
+    <form action="/add-product" method="post">
+      <input type="text" name="productName" placeholder="Product Name">
+      <input type="text" name="productSize" placeholder="Product Size">
+      <button type="submit">Add Product</button>
+    </form>
+  `);
+});
 
-app.get("/",(req,res)=>{
-   
+// Handle form submission
+app.post('/add-product', (req, res) => {
+const {productName,productSize}=req.body;
 
-  res.send("<h2>Hey ,this is Arish..</h2>")
-})
+  console.log('Product Name:', productName);
+  console.log('Product Size:', productSize);
 
-const port=3001;
-app.listen(port,()=>{
-  console.log(port);
-})
+  res.send('Product added successfully.');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
